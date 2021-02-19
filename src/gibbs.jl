7583a5925383ace78589bdbd415c14e34055e3fe
efmanu <manuedavakandam@gmail.com>
2021-02-20 06:32:18 +1100

    bug fix

--- src/gibbs.jl
+++ src/gibbs.jl
@@ -1,25 +1,27 @@
 function gibbs(proposal, logJoint::Function; itr = 100, burn_in = Int(round(itr*0.2))
 )	
 	states = Dict()
-	prev_params = rand.(proposal)
-	logJoint_prevs = logJoint(prev_params)
+	# prev_params = rand.(proposal)
+	param_val = copy(rand.(proposal))
+	logJoint_prevs = logJoint(param_val) #logJoint(prev_params)
 	logJoint_curs = 0.0
 	for i in 1:itr
-		acc = falses(length(prev_params))
-		param_val = copy(prev_params)
-		for (idx,val) in enumerate(proposal)			
-			param_val[idx] = rand(val)
+		states["itr_$i"] =  copy(param_val)#copy(Array(prev_params))
+		# param_val = copy(prev_params)
+		for (idx,val) in enumerate(proposal)	
+			prev_val = param_val[idx]	
+			param_val[idx] = copy(rand(val))
 			logJoint_curs = logJoint(param_val)
 			logα = logJoint_curs - logJoint_prevs
-			if (-Random.randexp() < logα)	
-				acc[idx] = true
+			if (-Random.randexp() < logα)
+				states["itr_$i"][idx] = copy(param_val[idx])	
+				# prev_params[idx] = copy(param_val[idx])
+				logJoint_prevs = logJoint_curs
+			else
+				param_val[idx] = prev_val
 			end
 		end
-		if(all(acc))
-			prev_params = copy(param_val)
-			logJoint_prevs = logJoint_curs
-		end
-		states["itr_$i"] = Array(prev_params)
+		
 	end
 
 	return MHSampler.data_formatting(states, burn_in, itr)
