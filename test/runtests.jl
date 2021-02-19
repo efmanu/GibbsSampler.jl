f132af5978bc59c76365334c7bf55685e4f5e648
efmanu <manuedavakandam@gmail.com>
2021-02-20 05:52:25 +1100

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using Test
-
+using GibbsSampler
+using Distributions
+using MHSampler
 @testset "gibbs_sampling" begin
-	a = 1
-	@test a == 1
+	
+	proposal = [Normal(2.0,3.0), Normal(3.0,3.0)]
+	function proposalf() 
+		return rand.(proposal)
+	end
+	priors = proposal
+
+	function logJoint(params)	
+		logPrior= sum(logpdf.(priors, params))
+		return logPrior
+	end
+	chn = GibbsSampler.gibbs(proposal, logJoint;itr = 10000)
+
+	chm = MHSampler.mhsample(proposalf, logJoint, itr = 10000)	
+	@test isapprox(mean(Array(chn[1,2:end])),mean(Array(chm[1,2:end])), atol=0.2)
 end
