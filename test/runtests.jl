b9d7f06e198e140c87419573be495e03879a5092
efmanu <manuedavakandam@gmail.com>
2021-02-24 00:35:19 +1100

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using AdvancedMH
 using MCMCChains
 using GibbsSampler
 using Distributions
-using MHSampler
 @testset "gibbs_sampling" begin
+	
 	
 	proposal = [Normal(2.0,3.0), Normal(3.0,3.0)]
 	function proposalf() 
@@ -16,10 +16,17 @@ using MHSampler
 		logPrior= sum(logpdf.(priors, params))
 		return logPrior
 	end
-	chn = GibbsSampler.gibbs(proposal, logJoint;itr = 10000)
 
-	chm = MHSampler.mhsample(proposalf, logJoint, itr = 10000)	
-	@test isapprox(mean(Array(chn[1,2:end])),mean(Array(chm[1,2:end])), atol=0.1)
+	# Construct a DensityModel.
+	mdl = DensityModel(logJoint)
+
+	# Set up our sampler with a joint multivariate Normal proposal.
+	spl = RWMH(MvNormal([2.0,3.0],3.0))
+
+	# Sample from the posterior.
+	chm = sample(mdl, spl, 100000; param_names=["μ", "σ"], chain_type=Chains)
+	chn = GibbsSampler.gibbs(proposal, logJoint;itr = 100000)
+	@test isapprox(mean(Array(chn[1,2:end])),mean(chm[:μ]), atol=0.1)
 end
 @testset "gibbs_likelihood" begin
 
@@ -36,9 +43,16 @@ end
 		logLikelihood = logpdf(Normal(model(params)), output)
 		return logPrior + logLikelihood
 	end
-	chn = GibbsSampler.gibbs(proposal, logJoint1;itr = 10000)
 
-	chm = MHSampler.mhsample(proposalf, logJoint1, itr = 10000)	
-	@show mean(Array(chn[1,2:end])) mean(Array(chm[1,2:end]))
-	@test isapprox(mean(Array(chn[1,2:end])),mean(Array(chm[1,2:end])), atol=0.7)
+	mdl1 = DensityModel(logJoint1)
+
+	# Set up our sampler with a joint multivariate Normal proposal.
+	spl1 = RWMH(MvNormal([1.0,0.0],5.0))
+
+	# Sample from the posterior.
+	chm = sample(mdl1, spl1, 100000; param_names=["μ", "σ"], chain_type=Chains)
+	chn = GibbsSampler.gibbs(proposal, logJoint1;itr = 100000)
+
+	@show mean(Array(chn[1,2:end])) mean(chm[:μ])
+	@test isapprox(mean(Array(chn[1,2:end])),mean(chm[:μ]), atol=0.1)
 end
